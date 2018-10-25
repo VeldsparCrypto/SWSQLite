fcf12156eeaf7edf9421b416ec08f17519a79323
Adrian Herridge <editfmah@yahoo.co.uk>
2018-10-25 17:10:35 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SWSQLite",
     targets: [Target(name: "SWSQLite", dependencies:[])],
-    dependencies: [.Package(url: "https://github.com/sharksync/CSQlite.git", majorVersion: 2)],
+    dependencies: [.Package(url: "https://github.com/sharksync/CSQlite.git", majorVersion: 1)],
     exclude: []
 )
 
