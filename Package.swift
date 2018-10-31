f2f42c3b7dd9b12a189bcae2973d1ad4765c0922
Adrian Herridge <editfmah@yahoo.co.uk>
2018-10-31 13:46:48 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,8 +2,22 @@ import PackageDescription
 
 let package = Package(
     name: "SWSQLite",
-    targets: [Target(name: "SWSQLite", dependencies:[])],
-    dependencies: [.Package(url: "https://github.com/sharksync/CSQlite.git", majorVersion: 1)],
-    exclude: []
+    products: [
+        .library        (name: "SWSQLite",     targets: ["SWSQLite"]),
+        ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/sharksync/CSQlite.git", .exact("1.0.2")),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", .exact("4.1.0")),
+        ],
+    targets: [
+        .target(
+            name: "SWSQLite",
+            dependencies: ["SwiftyJSON"],
+        ]
 )
+
+
+
 
