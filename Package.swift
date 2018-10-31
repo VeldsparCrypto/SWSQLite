d8c1f9e65123af37657872cc430553e9a73565d1
Adrian Herridge <editfmah@yahoo.co.uk>
2018-10-31 15:29:03 +0000

    fixed package manager

--- Package.swift
+++ Package.swift
@@ -2,21 +2,8 @@ import PackageDescription
 
 let package = Package(
     name: "SWSQLite",
-    products: [
-        .library(name: "SWSQLite", targets: ["SWSQLite"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/sharksync/CSQlite.git", .exact("1.0.2")),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", .exact("4.1.0")),
-    ],
-    targets: [
-        .target(
-            name: "SWSQLite",
-            dependencies: ["SwiftyJSON"]),
-    ]
+    targets: [Target(name: "SWSQLite", dependencies:[])],
+    dependencies: [.Package(url: "https://github.com/sharksync/CSQlite.git", majorVersion: 1), .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", versions: Version(4,1,0)..<Version(4,1,9))],
+    exclude: []
 )
-
-
-
-
 
