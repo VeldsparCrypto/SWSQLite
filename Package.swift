1601810623e6ac84f45a1fa1c2418ba0bdef6afd
Adrian Herridge <editfmah@yahoo.co.uk>
2018-11-02 13:10:08 +0000

    updated swifty

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SWSQLite",
     targets: [Target(name: "SWSQLite", dependencies:[])],
-    dependencies: [.Package(url: "https://github.com/sharksync/CSQlite.git", majorVersion: 1), .Package(url: "https://github.com/VeldsparCrypto/SwiftyJSON.git", versions: Version(4,1,0)..<Version(4,1,9))],
+    dependencies: [.Package(url: "https://github.com/sharksync/CSQlite.git", majorVersion: 1), .Package(url: "https://github.com/VeldsparCrypto/SwiftyJSON.git", versions: Version(4,1,0)..<Version(4,2,9))],
     exclude: []
 )
 
