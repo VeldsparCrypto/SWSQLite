87a1897a0ced6ef89586f3c0d9f1f7dca8f21e25
Adrian Herridge <editfmah@yahoo.co.uk>
2018-11-02 13:18:27 +0000

    removed missing module

--- Sources/SWSQLite.swift
+++ Sources/SWSQLite.swift
@@ -6,7 +6,6 @@
 
 import Dispatch
 import Foundation
-import SwiftyJSON
 
 internal let SQLITE_STATIC = unsafeBitCast(0, to: sqlite3_destructor_type.self)
 internal let SQLITE_TRANSIENT = unsafeBitCast(-1, to: sqlite3_destructor_type.self)
