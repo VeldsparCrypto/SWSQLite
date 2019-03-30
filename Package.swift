cbdde90303da64c68b8821719e0d42b012abe01f
Adrian Herridge <editfmah@yahoo.co.uk>
2019-03-30 06:20:17 +0000

    updates for swift 4+

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SWSQLite",
     targets: [Target(name: "SWSQLite", dependencies:[])],
-    dependencies: [.Package(url: "https://github.com/VeldsparCrypto/CSQlite.git", majorVersion: 1)],
+    dependencies: [.Package(url: "https://github.com/VeldsparCrypto/CSQlite.git", versions: Version(1,0,6)..<Version(2,0,0))],
     exclude: []
 )
