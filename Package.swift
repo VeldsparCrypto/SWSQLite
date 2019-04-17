6f16a55096994c2f744bb57756b677469b10e89b
Adrian Herridge <editfmah@yahoo.co.uk>
2019-04-17 15:20:56 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,11 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "SWSQLite",
     targets: [Target(name: "SWSQLite", dependencies:[])],
-    dependencies: [.Package(url: "https://github.com/VeldsparCrypto/CSQlite.git", versions: Version(1,0,6)..<Version(2,0,0))],
+    dependencies: [
+        .package(url: "https://github.com/VeldsparCrypto/CSQlite.git", .exact("1.0.8")),
+    ],
     exclude: []
 )
