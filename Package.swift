393672adbdce31770c9779ba95c5983b46ff17e0
Adrian Herridge <editfmah@yahoo.co.uk>
2019-04-17 15:32:57 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
     targets: [
         .target(
             name: "SWSQLite",
-            dependencies: []),
+            dependencies: [],
+            path: "./Sources"),
     ],
     swiftLanguageVersions: [
         4
