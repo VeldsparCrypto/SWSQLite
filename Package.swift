e163784099c1afd210295afb6ae710c505249851
Adrian Herridge <editfmah@yahoo.co.uk>
2019-04-17 15:22:39 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,5 +7,5 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/VeldsparCrypto/CSQlite.git", .exact("1.0.8")),
     ],
-    exclude: []
+        swiftLanguageVersions: [4]
 )
